44580344cfa640ba64ff4d8c4e1b38883a7581ab
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-03-28 12:37:05 -0600

    Imported things to the panel and added some declerations.
    The various imports will be used in the drawing app.

--- src/drawing/controller/DrawingRunner.java
+++ src/drawing/controller/DrawingRunner.java
@@ -1,5 +1,21 @@
 package drawing.controller;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.util.Hashtable;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;	//still need to addeven with javax.swing.*
+import javax.swing.event.ChangeListener;
+
+import drawing.controller.DrawingController;
+
 public class DrawingRunner
 {
 	public static void main(String [] args)
