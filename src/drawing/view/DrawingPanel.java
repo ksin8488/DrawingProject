03e58a672cc02a7cd1cd71a799cdb969d5eda169
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-03-28 13:02:12 -0600

    Initialized the various variables, created stubs for future methods, and coded the setupListeners().
    The setupListeners uses 2 HashTables (things that store 2 things side by side by using a key and the object) as well as set up things like the scale and edge lables, and the scale and edge sliders.

--- src/drawing/view/DrawingPanel.java
+++ src/drawing/view/DrawingPanel.java
@@ -25,5 +25,91 @@ public class DrawingPanel extends JPanel
 	
 	private DrawingController app;
 	
+	private SpringLayout appLayout;
+	private ShapeCanvas canvas;
+	private JPanel buttonPanel;
+	private JPanel sliderPanel;
+	private JSlider scaleSlider;
+	private JSlider edgeSlider;
+	private JButton triangleButton;
+	private JButton rectangleButton;
+	private JButton ellipseButton;
+	private JButton polygonButton;
+	private JButton clearButton;
+	private JButton saveButton;
+	private JButton colorButton;
 	
+	private int currentEdgeCount;
+	private int currentScale;
+	
+	public DrawingPanel(DrawingController app)
+	{
+		super();
+		this.app = app;
+		appLayout = new SpringLayout();
+		
+		currentScale = MINIMUM_SCALE;
+		currentEdgeCount = MINIMUM_EDGE;
+		scaleSlider = new JSlider(MINIMUM_SCALE, MAXIMUM_SCALE);
+		edgeSlider = new JSlider(MINIMUM_EDGE, MAXIMUM_EDGE);
+		
+		canvas = new ShapeCanvas(app);
+		sliderPanel = new JPanel();
+		buttonPanel = new JPanel(new GridLayout(0, 1));		//Turns the GUI into a grid that will be set with 0 rows and 1 column
+		
+		triangleButton = new JButton("add triangle");
+		rectangleButton = new JButton("add rectangle");
+		ellipseButton = new JButton("add ellipse");
+		polygonButton = new JButton("add polygon");
+		clearButton = new JButton("clear image");
+		saveButton = new JButton("save image");
+		colorButton = new JButton("change color");
+		
+		setupSliders();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	public void setupSliders()
+	{
+		
+	}
+	
+	public void setupPanel()
+	{
+		
+	}
+	
+	public void setupLayout()
+	{
+		
+	}
+	
+	public void setupListeners()
+	{
+		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>();
+		Hashtable<Integer, JLabel> edgeLabels = new Hashtable<Integer, JLabel>();
+		
+		scaleLabels.put(MINIMUM_SCALE, new JLabel("<HTLM>Small<BR>Shape</HTML>"));
+		scaleLabels.put((MAXIMUM_SCALE + MINIMUM_SCALE) / 2, new JLabel("<HTML>Medium<BR>Shape</HTML>"));
+		
+		edgeLabels.put(MINIMUM_EDGE,  new JLabel("Edges: " + MINIMUM_EDGE));
+		edgeLabels.put(MAXIMUM_EDGE, new JLabel("Edges: " + MAXIMUM_EDGE));
+		
+		scaleSlider.setLabelTable(scaleLabels);
+		scaleSlider.setOrientation(JSlider.VERTICAL);
+		scaleSlider.setSnapToTicks(true);
+		scaleSlider.setMajorTickSpacing(10);
+		scaleSlider.setPaintTicks(true);
+		scaleSlider.setPaintLabels(true);
+		
+		edgeSlider.setLabelTable(edgeLabels);
+		edgeSlider.setOrientation(JSlider.VERTICAL);
+		edgeSlider.setSnapToTicks(true);
+		edgeSlider.setMajorTickSpacing(3);
+		edgeSlider.setMinorTickSpacing(1);
+		edgeSlider.setPaintTicks(true);
+		edgeSlider.setPaintLabels(true);
+	}
 }
