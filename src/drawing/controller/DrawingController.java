67d0c0d4fbca57ccac5536bdcd6cfda7e251fbd1
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2018-04-26 12:16:30 -0600

    Added code to the DrawingController so some of the other things in other files work.
    There are still things like a getFrame method that still need work and such but hopefully these will help reduce some of the errors.

--- src/drawing/controller/DrawingController.java
+++ src/drawing/controller/DrawingController.java
@@ -1,9 +1,24 @@
 package drawing.controller;
 
+import drawing.view.DrawingFrame;
+import javax.swing.JOptionPane;
+
 public class DrawingController
 {
+	private DrawingFrame appFrame;
+	
+	public DrawingController()
+	{
+		appFrame = new DrawingFrame(this);
+	}
+	
 	public void start()
 	{
-		
+		JOptionPane.showMessageDialog(appFrame, "Welcome to art!");
+	}
+	
+	public void handleErrors(Exception error)
+	{
+		JOptionPane.showMessageDialog(appFrame,  error.getMessage());
 	}
 }
